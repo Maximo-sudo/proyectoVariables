3d84ffa58882cb50bf9f49c3eb4b69010fc4bdd6
Maximus <133859286+Maximo-sudo@users.noreply.github.com>
2025-01-14 09:46:43 +0100

    Primer commit
    Aqui Subo El Trabajo De El Entendimiento De Las Variables

---
+++ practicaVariables.js
@@ -0,0 +1,15 @@
+//Crea 4 variables para un perfil de usuario, imprímelas y cambia una de las variables antes de imprimirla nuevamente.
+
+let nombre = "maximo";
+let primerApellido = "vidal";
+let segundoApellido = "martinez";
+let edad = 19;
+
+console.log(nombre + " " + primerApellido + " " + segundoApellido + " " +  "tiene" + " " +  edad + " " +  "años");
+
+nombre = "Juan carlos";
+primerApellido = "sanchez";
+segundoApellido = "el cano";
+edad = 29;
+
+console.log(nombre + " " + primerApellido + " " + segundoApellido + " " +  "tiene" + " " +  edad + " " +  "años");
